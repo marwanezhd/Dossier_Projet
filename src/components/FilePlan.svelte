749ff626c29ec29eb38781c00d5e15ce74f0aa09
Marwane <marwane.zhd@gmail.com>
2024-04-19 16:55:40 +0200

    [add] SubComponent ->  CardMod, GithubProjectPage

---
+++ src/components/FilePlan.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<main>
+        Plan de dossier
+</main>
+
+<style>
+
+</style>
---
+++ src/components/GithubProjectPage.svelte
@@ -0,0 +1,13 @@
+<script>
+    let username = "marwanezhd";
+    let repo = "Dossier_Projet";
+    
+    function getRawGitHubURL(username, repo) {
+        console.log(`https://github.com/${username}/${repo}/raw/main/`)
+        return `https://github.com/${username}/${repo}/raw/main/index.html`;
+    }
+</script>
+
+<iframe title="github project" src={getRawGitHubURL(username, repo)} style="width: 100%; height: 100vh;"></iframe>
+
+<iframe  title="github project" src={getRawGitHubURL(username, repo)} width="100%" height="400" frameborder="0" scrolling="no"></iframe>
